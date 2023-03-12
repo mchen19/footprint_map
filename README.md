200c82de69c4dc87c32d7f9c74b5a6578e7e2eae
mchen19 <mchen19@163.com>
2023-03-12 21:16:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+# Special thanks
+
+- @[yihong0618](https://github.com/yihong0618) for [running_page](https://github.com/yihong0618/running_page)
+- @[ben-29](https://github.com/ben-29) for [workouts](https://github.com/ben-29/workouts_page)
---
+++ gatsby-config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: 'Footprint Map',
+    siteUrl: 'https://run.mchen.xyz',
+    logo: 'https://mchen.xyz/favicon.ico',
+    description: 'Personal Running & Riding Page',
+    keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://mchen.xyz',
+      },
+      {
+        name: 'About',
+        url: 'https://mchen.xyz/#about',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: "gatsby-plugin-vercel",
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https?:.*\/icons\/.*\.png/,
+            handler: `CacheFirst`,
+          },
+          {
+            urlPattern:
+              /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:\/\/api\.mapbox\.com\//,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:.*\/page-data\/.*\.json/,
+            handler: `StaleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+    },
+  ],
+};
